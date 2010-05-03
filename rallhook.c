80ebaf401896cced96f0d57e9304ff1dfba46823
Dario Seminara <rseminara@hotmail.com>
2010-05-02 22:17:30 -0300

    basic methods hook and unhook

--- rallhook.c
+++ rallhook.c
@@ -1,6 +1,24 @@
 #include <ruby.h>
 
+VALU rb_mRallHook;
+
+VALUE hook(VALUE self, VALUE hook_proc) {
+}
+
+VALUE unhook(VALUE self) {
+}
+
 void Init_rallhook() {
+
+	const char* initcode = 	"require 'rubygems'\n"
+						"require 'ruby-cymbol'\n";
+
+	rb_eval_string(initcode);
+
+	VALUE rb_mRallHook = rb_define_module("RallHook");
+	rb_define_singleton_method(rb_mRallHook, "hook", (RBHOOK*)(hook), 1);
+	rb_define_singleton_method(rb_mRallHook, "unhook", (RBHOOK*)(unhook), 0);
+
 
 }
 
