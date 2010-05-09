6794c0a728ff3cf57b7d4fb73b37dc8cfde89076
Dario Seminara <rseminara@hotmail.com>
2010-05-09 12:20:08 -0300

    delete innecesary function call_block_handle

--- ext/rallhook/rb_call_fake.c
+++ ext/rallhook/rb_call_fake.c
@@ -147,12 +147,6 @@ VALUE restore_hook_status_ensure(VALUE ary) {
 //	hook_enable_left = 0;
 }
 
-VALUE call_block_handle( VALUE parameter, VALUE* args) {
-	return rb_yield( args[0] );
-}
-
-
-
 VALUE rb_call_wrapper(VALUE ary){
 		VALUE* argv = (VALUE*)ary;
 		return rb_call_copy(CLASS_OF(rb_cRallHook), rb_cRallHook, id_call,5,argv,1,Qundef);
