8bfd82164eb0c8403fa38336fffb1f802bc5b054
Dario Seminara <rseminara@hotmail.com>
2010-05-11 20:35:11 -0300

    added -ldistorm64 in extconf.rb

--- ext/rallhook/extconf.rb
+++ ext/rallhook/extconf.rb
@@ -8,6 +8,8 @@ ruby_version = Config::CONFIG["ruby_version"]
 ruby_version = ruby_version.split(".")[0..1].join(".")
 end
 
+$LIBS = $LIBS + " -ldistorm64"
+
 if ruby_version == "1.8"
 	$CFLAGS = $CFLAGS + " -DRUBY1_8"
 elsif ruby_version == "1.9"
@@ -19,4 +21,4 @@ end
 
 create_makefile('rallhook')
 
-    
+
