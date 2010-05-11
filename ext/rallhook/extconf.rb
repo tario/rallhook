caabe209e0f15b558b46b863951c206350e104b6
Dario Seminara <dario@dario-desktop.(none)>
2010-05-11 14:43:42 -0300

    added detection of ruby version in extconf.rb and generate of corresponding macro definition to send to gcc in Makefile

--- ext/rallhook/extconf.rb
+++ ext/rallhook/extconf.rb
@@ -1,6 +1,21 @@
 require 'mkmf'
 dir_config('rallhook')
 CONFIG['CC'] = 'gcc'
+
+ruby_version = ARGV[0]
+unless ruby_version
+ruby_version = Config::CONFIG["ruby_version"]
+end
+
+if ruby_version == "1.8"
+	$CFLAGS = $CFLAGS + " -DRUBY1_8"
+elsif ruby_version == "1.9"
+	$CFLAGS = $CFLAGS + " -DRUBY1_9"
+else
+	print "ERROR: unknown ruby version: #{ruby_version}\n"
+	print "try passing the rubyversion by argument (1.8 or 1.9)\n"
+end
+
 create_makefile('rallhook')
 
     
