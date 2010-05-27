e7d4794bf71e367dc1e7bb708d0463d242f0db50
Dario Seminara <rseminara@hotmail.com>
2010-05-27 11:56:51 -0300

    added facade files, module ruby_redirect

---
+++ ext/rallhook_base/ruby_redirect.h
@@ -0,0 +1,39 @@
+/*
+
+This file is part of the rallhook project, http://github.com/tario/rallhook
+
+Copyright (c) 2009-2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+rallhook is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+rallhook is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with rallhook.  if not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef __RUBY_REDIRECT_H
+#define __RUBY_REDIRECT_H
+
+// Acts as facade of the division between the ruby interpreter modification
+
+#include <ruby.h>
+
+typedef struct _CallData {
+	VALUE klass;
+	VALUE recv;
+	ID mid;
+	VALUE args;
+} CallData;
+
+typedef VALUE (*REDIRECTHANDLER) ( CallData* call_data );
+
+int put_redirect_handler( REDIRECTHANDLER* redirect_handler);
+
+#endif
