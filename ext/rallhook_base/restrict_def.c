53fb8d2598dcb4541158aed8369869033a7ef8c5
Dario Seminara <rseminara@hotmail.com>
2010-05-29 00:17:15 -0300

    added restrict_def module

---
+++ ext/rallhook_base/restrict_def.c
@@ -0,0 +1,69 @@
+/*
+
+This file is part of the rallhook project, http://github.com/tario/rallhook
+
+Copyright (c) 2009-2010 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+rallhook is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+rallhook is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with rallhook.  if not, see <http://www.gnu.org/licenses/>.
+
+*/
+#include "ruby.h"
+#include "restrict_def.h"
+#include "ruby_symbols.h"
+#include "hook.h"
+
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
+#include <dlfcn.h>
+
+
+int restrict_def = 0;
+
+typedef void NODE_;
+
+typedef void (*RBADDMETHOD) (
+    VALUE klass,
+    ID id,
+    NODE_* node,
+    int noex
+);
+
+RBADDMETHOD rb_add_method_copy;
+
+void rb_add_method_fake(
+    VALUE klass,
+    ID id,
+    NODE_* node,
+    int noex
+) {
+	rb_add_method_copy(klass,id,node,noex);
+}
+
+
+void init_restrict_def() {
+	void* handle = dlopen(current_libruby(),0x101);
+	char* rb_funcall = (char*)dlsym(handle, "rb_funcall");
+	Dl_info info;
+	dladdr(rb_funcall, &info);
+
+	unsigned char* base = (unsigned char*)info.dli_fbase;
+
+	void* rb_add_method_original = ruby_resolv(base,"rb_add_method");
+
+	int inst_size = get_instructions_size(rb_add_method_original, 256);
+	rb_add_method_copy = put_jmp_hook(rb_add_method_original, rb_add_method_fake, inst_size);
+
+}
