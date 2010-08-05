3fd08bdb77bc1e00496a43f6b9b435dece5ba052
Dario <darios3@gmail.com>
2010-08-04 23:14:51 -0300

    calling hook_rb_add_method before enable_redirect

--- ext/rallhook_base/rallhook.c
+++ ext/rallhook_base/rallhook.c
@@ -241,9 +241,9 @@ VALUE hook(VALUE self, VALUE hook_proc) {
 
 	put_redirect_handler( rallhook_redirect_handler );
 
-	enable_redirect(tinfo_from_thread(rb_thread_current()));
-
 	hook_rb_add_method();
+
+	enable_redirect(tinfo_from_thread(rb_thread_current()));
 
 	if (rb_block_given_p() ) {
 		return rb_ensure(rb_yield, Qnil, unhook, self);
